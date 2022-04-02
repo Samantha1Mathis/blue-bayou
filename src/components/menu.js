cba4973fdd3110ac492c398921dc435e9bc4f652
Etienne Thompson <et@etiennethompson.com>
2022-04-02 11:42:32 -0700

    Add navigation to the menu.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,65 @@
 import React from "react";
+import { Tabs, Tab, Navbar, Container, Nav } from "react-bootstrap";
+import { HashLink as Link } from "react-router-hash-link";
 import menuData from "../data/menu";
 import "./menu.css";
 
 export function Menu() {
   let lunchKeys = Object.keys(menuData.Lunch);
+  let dinnerKeys = Object.keys(menuData.Dinner);
+
+  let [activeKeys, setActiveKeys] = React.useState(lunchKeys);
+  let [currentMenu, setCurrentMenu] = React.useState("Lunch");
+
+  let changeActiveKeys = (title) => {
+    setCurrentMenu(title);
+    setActiveKeys(title === "Lunch" ? lunchKeys : dinnerKeys);
+  };
 
   return (
     <div className="menu-container">
       <h1>Menu</h1>
-      {lunchKeys.map((key, index) => {
+      <Tabs
+        onSelect={(eventKey) => changeActiveKeys(eventKey)}
+        defaultActiveKey="Lunch"
+        id="time-of-day-menu"
+      >
+        <Tab eventKey="Lunch" title="Lunch"></Tab>
+        <Tab eventKey="Dinner" title="Dinner"></Tab>
+      </Tabs>
+
+      <Navbar className="menu-item-container" bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav style={{ maxHeight: "100px" }} navbarScroll>
+              {activeKeys.map((key, index) => {
+                return (
+                  <Link
+                    key={`menu-item-key-${key}-${index}`}
+                    className="menu-item-field"
+                    to={`#${key}`}
+                  >
+                    {key}
+                  </Link>
+                );
+              })}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {activeKeys.map((key, index) => {
         return (
           <>
-            <h3 className="menu-section-header" key={`menu-key-${index}`}>
+            <h3
+              className="menu-section-header"
+              id={key}
+              key={`menu-key-${index}`}
+            >
               {key}
             </h3>
-            {menuData.Lunch[key].map((meal, mealIndex) => {
+            {menuData[currentMenu][key].map((meal, mealIndex) => {
               return (
                 <div className="meal-container" key={`meal-key-${mealIndex}`}>
                   <div className="meal-row">

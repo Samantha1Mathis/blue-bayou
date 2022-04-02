52a1d5e93ac372d4810df037d00dccb02132025f
Etienne Thompson <et@etiennethompson.com>
2022-04-01 20:29:28 -0700

    Image example.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,10 @@
 import React from "react";
+import img3 from "../images/home-images/img3.jpg";
 
 export default function Home() {
   return (
     <div>
-      <img
-        className="d-block w-100"
-        src="../images/home-images/img3.jpg"
-        alt="3"
-      />
+      <img className="d-block w-100" src={img3} alt="3" />
     </div>
   );
 }

5a4cbf1fffdbe0ba3414a9e7a871c4ba2b106808
Etienne Thompson <et@etiennethompson.com>
2022-04-02 12:00:23 -0700

    Fix menu component and make menu page.

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import { Menu } from "../components/menu";
 
 export default function MenuPage() {
-  return <Menu />;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        marginTop: "10px",
+        marginBottom: "10px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card style={{ width: "60%", padding: "10px" }}>
+        <Menu />
+      </Card>
+    </div>
+  );
 }

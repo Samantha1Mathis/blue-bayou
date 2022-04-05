0f8d279c2af8d9d616a60f2369b344b9e989a98f
Etienne Thompson <et@etiennethompson.com>
2022-04-04 17:14:39 -0700

    Add menu and indicator for scrolling.

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Menu } from "../components/menu";
+import { NavbarCustom } from "../components/navbar";
 
 export default function MenuPage() {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        marginTop: "10px",
-        marginBottom: "10px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Card style={{ width: "60%", padding: "10px" }}>
-        <Menu />
-      </Card>
-    </div>
+    <>
+      <NavbarCustom />
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          marginTop: "10px",
+          marginBottom: "10px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card style={{ width: "60%", padding: "10px" }}>
+          <Menu />
+        </Card>
+      </div>
+    </>
   );
 }
